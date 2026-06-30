06f4c2413aa8bb60719a3378e5c73f73372c0b2e
kietng.23it@vku.udn.vn <kietnguyengia20005@gmail.com>
2026-06-30 16:37:15 +0700

    feat(analytics): add dashboard UI

--- src/modules/auth/pages/ForgotPasswordPage.js
+++ src/modules/auth/pages/ForgotPasswordPage.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "../styles/ForgotPasswordPage.css";
-import Brand from "../components/Brand";
+import Brand from "../../../public-site/components/Brand";
 import InputField from "../components/InputField";
 
 function ForgotPasswordPage() {
